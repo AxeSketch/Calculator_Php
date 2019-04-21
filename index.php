e5f927db4c7e11626a36a3b0b06a678af0320690
unknown <axesketch@gmail.com>
2019-04-21 22:35:39 +0300

    commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="Num1" placeholder="Primul numar">
+        <input type="text" name="Num2" placeholder="Al doilea numar">
+        <select name="" id="">
+            <option value="">Alege operatia</option>
+            <option value="">Aduna</option>
+            <option value="">Scade</option>
+            <option value="">Inmulteste</option>
+            <option value="">Scade</option>
+
+        </select>
+    </form>
+</body>
+</html>
